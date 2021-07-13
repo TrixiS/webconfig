a712c4d8221edc4964772c120f6ed75889282f78
TrixiS <oficialmorozov@gmail.com>
2021-07-13 20:25:00 +0500

    Forward ref in Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,16 @@
 import * as React from "react";
 
-export default function Input({
-  className,
-  ...rest
-}: React.InputHTMLAttributes<HTMLInputElement>) {
-  return (
-    <input
-      className={
-        "rounded-lg border border-gray-200 py-1 px-2 focus:outline-none focus:border-gray-400"
-      }
-      {...rest}
-    />
-  );
-}
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => (
+  <input
+    className={
+      "rounded-lg border border-gray-200 py-1 px-2 focus:outline-none focus:border-gray-400"
+    }
+    {...props}
+    ref={ref}
+  />
+));
+
+export default Input;
