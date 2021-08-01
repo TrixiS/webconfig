dfb6de6df0ecea19da3756cfe5c7384affba4c4a
TrixiS <oficialmorozov@gmail.com>
2021-08-01 23:20:51 +0500

    Provide download and delete buttons in TextField

--- src/components/LogsPage.tsx
+++ src/components/LogsPage.tsx
@@ -4,7 +4,22 @@ import Page, { PageProps } from "./Page";
 import { apiUrl } from "../App";
 
 export default function LogsPage({ children, ...rest }: PageProps) {
-  const { data: logsData } = useFetcher<Logs>(`${apiUrl}/logs`);
+  const { data: logsData } = useFetcher<Logs>(`${apiUrl}/logs`, {
+    refreshInterval: 5000,
+  });
+
+  const handleDelete = () => fetch(`${apiUrl}/logs`, { method: "DELETE" });
+
+  const handleDownload = async () => {
+    if (!logsData) return;
+
+    const fileLink = document.createElement("a");
+    const url = window.URL.createObjectURL(new Blob([logsData.text]));
+    fileLink.href = url;
+    fileLink.setAttribute("download", "logs.txt");
+    fileLink.click();
+    fileLink.parentNode?.removeChild(fileLink);
+  };
 
   return (
     <Page {...rest}>
@@ -12,7 +27,9 @@ export default function LogsPage({ children, ...rest }: PageProps) {
         className="outline-none"
         value={logsData?.text}
         readOnly={true}
-        rows={33}
+        rows={32}
+        onDelete={handleDelete}
+        onDownload={handleDownload}
       />
       {children}
     </Page>
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,15 +1,41 @@
-import { TextareaHTMLAttributes } from "react";
+import * as React from "react";
+import { downloadIcon, trashIcon } from "../icons";
 
 export default function TextField({
   className,
+  children,
+  onDownload,
+  onDelete,
   ...rest
-}: TextareaHTMLAttributes<HTMLTextAreaElement>) {
+}: TextFieldProps) {
   return (
-    <textarea
-      className={`w-full rounded-lg p-2 bg-black text-white font-mono resize-none ${
-        className ?? ""
-      }`}
-      {...rest}
-    />
+    <div className="w-full rounded-lg text-white bg-black p-2">
+      <textarea
+        className={`w-full bg-black font-mono resize-none ${className ?? ""}`}
+        {...rest}
+      >
+        {children}
+      </textarea>
+      <div className="inline-flex">
+        <button
+          className="opacity-50 hover:opacity-100 animate-pulse"
+          onClick={onDownload}
+        >
+          {downloadIcon}
+        </button>
+        <button
+          className="opacity-50 hover:opacity-100 hover:text-red-500"
+          onClick={onDelete}
+        >
+          {trashIcon}
+        </button>
+      </div>
+    </div>
   );
+}
+
+export interface TextFieldProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  onDownload?: () => Promise<any>;
+  onDelete?: () => Promise<any>;
 }
