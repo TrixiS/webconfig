6d061c97386663835c3938999490b0b538774e03
TrixiS <oficialmorozov@gmail.com>
2021-08-01 23:19:01 +0500

    Take useSWR args in useFetcher as well

--- src/lib/useFetcher.ts
+++ src/lib/useFetcher.ts
@@ -1,12 +1,18 @@
 import useSWR, { SWRResponse } from "swr";
+import { Configuration } from "swr/dist/types";
 
 const fetcher = (input: any, ...args: any[]) =>
   fetch(input, ...args).then((res) => res.json());
 
 export default function useFetcher<Data = any, Error = any>(
-  route: string
+  route: string,
+  useSWRConfiguration?: Partial<Configuration<Data, Error>>
 ): UseFetcherResponse<Data, Error> {
-  const { data, error, ...rest } = useSWR<Data, Error>(route, fetcher);
+  const { data, error, ...rest } = useSWR<Data, Error>(
+    route,
+    fetcher,
+    useSWRConfiguration
+  );
 
   return {
     data,
